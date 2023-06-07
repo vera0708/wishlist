eef5e82381a60ad05c1eb2aa2105bab163f672dd
vera0708 <verasdee@gmail.com>
2023-06-07 22:22:32 +0700

    finish project, add .gitignore

--- scripts/const.js
+++ scripts/const.js
@@ -1,6 +1,5 @@
 // export const API_URL = 'http://localhost:3000';
 export const API_URL = 'https://fluoridated-puzzling-coast.glitch.me';
-// export const API_URL = 'https://billowy-dolomite-licorice.glitch.me';
-// https://fluoridated-puzzling-coast.glitch.me/
+
 export const JWT_TOKEN_KEY = 'WishList';
 export const ROUTE_NEW_WISH = 'newwish';
